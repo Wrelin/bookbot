d08109cb84b8ed587555941c71c13ed4410d8836
Александр Журавлёв <aleksandr.zhuravlev@maximumtest.ru>
2025-09-02 21:25:17 +0300

    finish

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import stats as s
 
 def get_book_text(filepath):
@@ -5,12 +6,25 @@ def get_book_text(filepath):
         return f.read()
 
 def main():
-    frankenstein_path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    frankenstein_path = sys.argv[1]
     frankenstein_text = get_book_text(frankenstein_path)
 
+    words_count = s.get_words_count(frankenstein_text)
     char_count = s.get_characters_count(frankenstein_text)
+    letters = s.get_letters_list(char_count)
 
-    print(char_count)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {frankenstein_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {words_count} total words")
+    print("--------- Character Count -------")
+    for letter in letters:
+        print(f"{letter['char']}: {letter['num']}")
+    print("============= END ===============")
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -10,9 +10,20 @@ def get_characters_count(text):
     result = {}
 
     for c in lower_characters:
+        if not c.isalpha():
+            continue
+
         if c not in result:
             result[c] = 0
         result[c] += 1
 
     return result
 
+def get_letters_list(counts_dict):
+    result = []
+    for letter in counts_dict:
+        dct = {"char": letter, "num": counts_dict[letter]}
+        result.append(dct)
+
+    result.sort(reverse=True, key=lambda items: items["num"])
+    return result
